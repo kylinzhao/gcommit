8dda69b77712cd61e33243095dd9ab0f39a156cc
xuezhi.tian <xuezhi.tian@qunar.com>
2016-03-16 17:39:13 +0800

    changelog显示顺序从新到旧

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 
 var fs = require('fs'),
     child_process = require('child_process'),
-    lineReader = require('line-reader'),
+    prependFile = require('prepend-file'),
     params = [],
     comments = '',
     type = '',
     filePath = 'CHANGELOG.md',
     gitList = ['b','c','clone','init','add','mv','reset','rm','bisect','grep','log','show','status','branch','checkout','commit','diff','merge','rebase','tag','fetch','pull','push'];
+
+var verReg = /\#\#\#\#\#\s\d+\.\d+\.\d+\.(\d)+/g;
+var verLineCount = 0; // verReg 计数器，记录其在数据中的索引
 
 var argvsMap = {};
 var argvs = (function(){
@@ -32,14 +35,14 @@ function initParams(){
                     var branch = getValue(key);
                     getNewVersion().then(function(newVersion){
                         console.log('新建分支', newVersion);
-                        _write('\n' + newVersion + '\n',function(){
+                        _write('addver', '\n' + newVersion + '\n',function(){
                             reslove();
                         });
                     });
                 }else if(key === 'commit' || key === 'c'){
                     var commit = getValue(key)
                     console.log('提交修改', commit);
-                    _write(commit && '* ' + commit || '',function(){
+                    _write('commit', commit && '* ' + commit || '',function(){
                         reslove();
                     });
                 }else{
@@ -55,19 +58,64 @@ function initParams(){
     });
 }
 
-function _write(val,callback){
+function _writeCommit(val, callback) {
+    var lines = fs.readFileSync(filePath, 'utf8').split('\n');
+    for(var i = 0; i < lines.length; i++) {
+        var line = lines[i];
+        if(line.match(verReg)) {
+            verLineCount++;
+            if(verLineCount === 2) {
+                lines.splice((i - 1), 0, val);
+                var text = lines.join('\n');
+                fs.writeFile(filePath, text, function(err, res) {
+                    if(err) return console.log(err);
+                    callback && callback();
+                });
+                return;
+            }
+        }
+    }
+
+    if(verLineCount === 1) {
+        fs.appendFile(filePath, '\n' + val, 'utf8', function(err) {
+            if(err) throw err;
+            callback && callback();
+        });
+    } else {
+        var ret = [];
+        ret.push('\n\n' + getLastesVer(filePath) + '\n');
+        ret.push(val);
+        fs.appendFile(filePath, ret.join('\n'), 'utf8', function(err) {
+            if(err) throw err;
+            callback && callback();
+        })
+    }
+}
+
+function _write(type, val,callback){
+    var typeMap = {
+        'addver': prependFile,
+        'commit': fs.appendFile
+    }
     if(!val){
         console.log('未输入修改信息');
         callback && callback();
         return;
     }
-    fs.appendFile(filePath, val + '\n' ,'utf8',function(err){
-        if (err){
-            throw err;
-        }else{
-            callback && callback();
-        }
-    });
+
+    if(type === 'addver') {
+        prependFile(filePath, '\n' + val ,'utf8',function(err){
+            if (err){
+                throw err;
+            }else{
+                callback && callback();
+            }
+        });   
+    }
+    if(type === 'commit') {
+        _writeCommit(val, callback);
+    }
+
 }
 
 function getValue(key){
@@ -89,40 +137,32 @@ function getValue(key){
     return value;
 }
 
+function getLastesVer(filePath) {
+    var lineArr = fs.readFileSync(filePath, 'utf8').split('\n');
+    var ver = '##### 1.0.0.0';
+
+    for(var i = 0, len = lineArr.length; i < len; i++) {
+        var line = lineArr[i];
+        if(line && line.match(verReg)) {
+            var position = line.lastIndexOf('.');
+            var localVer = line.substr(position+1).match(/\d+/);
+            localVer = localVer && localVer[0] || 0;
+            localVer = parseInt(localVer,10) + 1;
+            var nativeVer = line.substr(0,position+1);
+            ver = nativeVer + localVer;
+            return ver;
+        }
+    }
+
+    return ver;
+}
+
 function getNewVersion(){
     var ver = '';
     var p = new Promise(function(reslove,reject){
         fs.exists(filePath,function(bool){
             if(bool){
-                ver = '';
-                var flag = false;
-                lineReader.eachLine(filePath, function(line, last){
-                    if(line && line.match(/\#\#\#\#\#\s\d+\.\d+\.\d+\.(\d)+/g)){
-                        var position = line.lastIndexOf('.');
-                        var localVer = line.substr(position+1).match(/\d+/);
-                        localVer = localVer && localVer[0] || 0;
-                        localVer = parseInt(localVer,10) + 1;
-                        var nativeVer = line.substr(0,position+1);
-                        ver = nativeVer + localVer;
-                        flag = true;
-                    }
-
-                    if(last && !flag){
-                        ver = '##### 1.0.0.0';
-                        flag = true;
-                    }
-
-                    if(last){
-                        reslove(ver);
-                    }
-                });
-
-                setTimeout(function(){
-                    if(!flag){
-                        reslove('##### 1.0.0.0');
-                    }
-                },100)
-
+                reslove(getLastesVer(filePath));
             }else{
                 // 1.0.0 = native 版本，最后一位是本地版本
                 ver = '##### 1.0.0.0';
